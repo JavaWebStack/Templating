a86b3efaccd3242e4af80996e22547bea0bb2ba9
Julian Gojani <julian@gojani.xyz>
2021-02-20 16:08:32 +0100

    Added default render(name, Object)

--- Templating-Engine/src/main/java/org/javawebstack/templating/TemplateEngine.java
+++ Templating-Engine/src/main/java/org/javawebstack/templating/TemplateEngine.java
@@ -1,9 +1,16 @@
 package org.javawebstack.templating;
 
+import org.javawebstack.abstractdata.AbstractElement;
+import org.javawebstack.abstractdata.AbstractObject;
+
 import java.util.Map;
 
 public interface TemplateEngine {
 
     String render(String name, Map<String, Object> data) throws TemplateException;
+
+    default String render(String name, Object data) {
+        return render(name, AbstractElement.fromAbstractObject(data).toTree());
+    }
 
 }
